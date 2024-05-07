2c0352322261d3b6f2baa035eec3e9a68e744360
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-05-07 20:02:39 +0800

    提交第一次

---
+++ test1/src/main/java/test.java
@@ -0,0 +1,12 @@
+/**
+ * @Program: test1   //名称
+ * @ClassName : test  //类名
+ * @Author: lenyan   /作者
+ * @Date: 2024/5/7  20:02
+ **/
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
